94cb196dd2b38ae7ad1d522c1edebca211c1eb9f
c0gnit0x <sktman23@outlook.com>
2025-11-06 00:03:33 -0700

    edits in comments
    Refactor comments for clarity and consistency in the Cipher class.

--- Cipher.py
+++ Cipher.py
@@ -3,12 +3,10 @@
 # Created Nov 5, 2025
 
 class Cipher:
-    '''
     # defining cipher class for encryption and decryption
     # blueprint for creating Cipher objects that performs two functions: 
         # Encrypt text by shifting letters forward in the alphabet
         # Decrypt by shifting letters backward to get to original message 
-    '''
     '''
         Attributes:
             # __shift (int): Number shifts for each letter
@@ -19,48 +17,33 @@ class Cipher:
      '''
     
     def __init__(self, shift = 3, case_sensitive = True):
-        '''# the construction method that will happen everytime there is new cipher object
-           # initializing cipher attributes 
-        '''
-        
+        # the construction method that will happen everytime there is new cipher object
+        # initializing cipher attributes 
+                
         self.__shift = shift
-        ''' # creating the attribute called __shift and storing shift value
+            # creating the attribute called __shift and storing shift value
             # self. attaches the number of shifts to the current cipher object
             # __ means this attribute's name is mangled/extra private
             # shift gets assigned number inputted by user
-         '''
-            
+                 
         self._alphabet_upper = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' 
-        '''# creates and stores Russian _alphabet_upper 
-           # string that contains all cyrillic upper case letters 
-           # _ makes it private
-         '''
-
+            # creates and stores Russian _alphabet_upper 
+            # string that contains all cyrillic upper case letters 
+            # _ makes it private
+         
         self._alphabet_lower = 'абвгдежзийклмнопрстуфхцчшщъыьэюя' 
-        '''# creates and stores Russian lowercase alphabet
-           # string that contains all cyrillic lower case letters 
-        ''' 
-
+            # creates and stores Russian lowercase alphabet
+            # string that contains all cyrillic lower case letters 
+       
         self._case_sensitive = case_sensitive 
-        '''# stores user preference for keeping same case as original text 
-           # True = keep as original case 
-           # False = convert to all uppercase upon encryption
-        ''' 
+            # stores user preference for keeping same case as original text 
+            # True = keep as original case 
+            # False = convert to all uppercase upon encryption 
 
         self._history = []
-        '''# creating history log attribute 
-           # will store all encrypted and decrypted messages 
-           # initializing it with empty list 
-        '''
-    
-           
-
-        
-        
-        
-        
-
-       
+            # creating history log attribute 
+            # will store all encrypted and decrypted messages 
+            # initializing it with empty list        
 
     '''
         Note to coders: Only use private or mangled methods. The only public methods should be encrypt or decrypt. All methods must start with one or two underscores.
