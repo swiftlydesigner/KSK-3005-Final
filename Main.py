c262ab4cdf3bd324813bc925c50228f5daf0ebbd
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-11-05 19:07:25 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+# Koa Hawn
+# Sarah Tillman
+# Kyle Parker
+
+# Final Project - 3005
+# Autumn Quarter 2025
+# Due Nov 14, 2025
+
+def main():
+    # Code starts here
+    pass
+
+if __name__ == "__main__":
+    main()
