c5da1aedd6d5609eafb73ede41be28a46661bc64
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-11-05 22:10:51 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,8 +6,16 @@
 # Autumn Quarter 2025
 # Due Nov 14, 2025
 
+# Created Nov 5, 2025
+# Modified Nov 5, 2025
+
+import Cipher
+
 def main():
     # Code starts here
+    cipher = Chipher.Cipher()
+    cipher.encrypt("")
+    cipher.decrypt("")
     pass
 
 if __name__ == "__main__":
