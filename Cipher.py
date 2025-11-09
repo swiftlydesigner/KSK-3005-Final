06bdab4724cb1d8c064d583ccde0858f9659a316
c0gnit0x <sktman23@outlook.com>
2025-11-09 02:28:37 -0700

    updated main and cipher
    please add more or correct anything and put notes so i can learn .

--- Cipher.py
+++ Cipher.py
@@ -6,6 +6,8 @@
 # This class will be responsible for encryption and decryption.
 # Created Nov 5, 2025
 
+import csv 
+
 class Cipher:
     # defining cipher class for encryption and decryption
     # blueprint for creating Cipher objects that performs two functions: 
@@ -38,6 +40,14 @@ class Cipher:
         self._alphabet_lower = 'абвгдежзийклмнопрстуфхцчшщъыьэюя' 
         # creates and stores Russian lowercase alphabet
         # string that contains all cyrillic lower case letters 
+        
+        self._alphabet_upper += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        # creates and stores latin uppercase alphabet 
+        # string that contains all latin upper case letters 
+        
+        self._alphabet_lower += 'abcdefghijklmnopqrstuvwxyz'
+        # creates and stores latin lower case alphabet
+        # string that contains all latin lower case letters 
        
         self._case_sensitive = case_sensitive 
         # stores user preference for keeping same case as original text 
@@ -50,10 +60,9 @@ class Cipher:
         # initializing it with empty list        
 
     def encrypt(self, message):
-        '''Takes a plain-text message and returns an excrypted message.'''
+        '''Takes a plain-text message and returns an encrypted message.'''
         # defining method called encrypt for cipher class that takes one argument- message 
         # each letter shifts forward by __shifts
-        # non cyrillic alphabet characters remain as is/unchanged 
         # case sensitivity is decided based on user in _case_sensitive T/F selection
 
         encrypted = [] 
@@ -64,9 +73,9 @@ class Cipher:
             # need to loop through each character individually to transform them 
         
             if char in self._alphabet_upper: 
-                # checking if character is in upper case cyrillic 
+                # checking if character is in upper case cyrillic or latin
                 # checks against the string containing all upper case cyrillic self._alphabet_upper 
-                # only want to apply encryption to cyrillic messages, latin characters, numbers, punctuation unchanged 
+                # only want to apply encryption to cyrillic and latin characters, numbers, punctuation unchanged 
                 
                 index = self._alphabet_upper.index(char)
                 # this finds the index position of upper case letter in message string 
@@ -75,7 +84,7 @@ class Cipher:
                 # taking current index and calculating new index position with shift 
                 # self.__shift, mangled attribute- the number of positions to move forward in alphabet 
                 # adding shift to current position 
-                # len gives total number of cyrillic alphabet upper case- 33 
+                # len gives total number of cyrillic and Latin alphabet upper case
                 # modulus operator makes sure that the cipher stays in alphabet range determined by len
                 # if end of alphabet is reached it will wrap back to beginning 
                         
@@ -85,8 +94,8 @@ class Cipher:
                 # resulting char is set to encrypted_char for new letter that will go in final encrypted message 
                         
             elif char in self._alphabet_lower:  
-                # checking if character is in lowercase cyrillic if wasnt uppercase char
-                # using self_alphabet_lower to compare against- contains all lowercase cyrillic 
+                # checking if character is in lowercase cyrillic or latin if wasnt uppercase char
+                # using self_alphabet_lower to compare against- contains all lowercase cyrillic or latin
                 # will not change latin char, numbers, punctuation 
 
                 index = self._alphabet_lower.index(char)
@@ -101,11 +110,11 @@ class Cipher:
                 # gets encrypted letter lower case from shifted index
                 
             else:
-                # if any letters not in cyrillic 
-                # only encrypting Russian messages 
+                # if any letters not in cyrillic or latin
+                # only encrypting Russian and latin messages 
                  
                 encrypted_char = char
-                # if not cyrillic then character in message remains the same 
+                # if not cyrillic or latin then character in message remains the same 
                 # no change 
                          
             encrypted.append(encrypted_char)
@@ -124,7 +133,7 @@ class Cipher:
             # will default to all upper case letters 
             # keeps it consistent since no original case was needed   
                 
-        self._history.append(("Encrypt", message, encrypted_message))
+        self._history.append(("Зашифровать: (encrypt)", message, encrypted_message))
         # tracking the encryption process in a log 
         # "Encrypt" is the operation happening 
         # message- original message inputted 
@@ -140,7 +149,7 @@ class Cipher:
         '''Takes an encrypted message and returns a plain text message. '''
         # defining method called decrypt for cipher class- takes one arg- message 
         # each letter will shift backwards by __shift
-        # non cyrillic alphabet characters remain as is/unchanged 
+        # non cyrillic and latin alphabet characters remain as is/unchanged 
         # case sensitivity is decided based on user in _case_sensitive T/F selection
 
         decrypted = []
@@ -150,7 +159,7 @@ class Cipher:
             # looping through each individual character in message 
 
             if char in self._alphabet_upper:
-                # checking if character is uppercase cyrillic letter
+                # checking if character is uppercase cyrillic or latin letter
 
                 index = self._alphabet_upper.index(char)
                 # getting the index of char in uppercase alphabet
@@ -164,7 +173,7 @@ class Cipher:
                 # getting original char from new_index  
 
             elif char in self._alphabet_lower:
-                # checking if character is lowercase cyrillic 
+                # checking if character is lowercase cyrillic or latin
 
                 index = self._alphabet_lower.index(char)
                 # getting index of letter in lower case alphabet 
@@ -177,11 +186,11 @@ class Cipher:
                 # getting original character from new_index 
 
             else:
-                # if any letters not in cyrillic 
-                # only encrypting Russian messages 
+                # if any letters not in cyrillic or latin characters 
+                # only encrypting Russian and Latin messags 
                  
                 decrypted_char = char
-                # if not cyrillic then character in message remains the same 
+                # if not cyrillic or latin then character in message remains the same 
                 # no change
 
             decrypted.append(decrypted_char)
@@ -198,7 +207,7 @@ class Cipher:
             decrypted_message = decrypted_message.upper()
             # will convert the message to uppercase by default 
 
-        self._history.append(("Decrypt", message, decrypted_message))
+        self._history.append(("Расшифровать: (decrypt)", message, decrypted_message))
         # logs the decryption in _history list
         # Decrypt is what happened 
         # message is encrypted version 
@@ -206,41 +215,96 @@ class Cipher:
 
         return decrypted_message
         # returns original message
-        
+              
     def show_history(self):
-            '''Displays the history of all encryption and decryption operations.'''
-            # defining method in cipher class 
-            # self gives access to _history because it is an attribute
-            # will print record of each cipher operation
+        '''Displays the history of all encryption and decryption operations.'''
+        # defining method in cipher class 
+        # self gives access to _history because it is an attribute
+        # will print record of each cipher operation
+
+        for cipher_op, message, result in self._history:
+            # loops through info stored in self._history
+            # unpacking tuples into three variables 
+
+            print(f"{cipher_op}:\n")
+            # printing cipher operation ie- Encrypt or Decrypt 
+
+            print(f" Сообщение: (message) {message}\n")
+            # starting message that operation will be performed on 
+
+            print(f" Результат: (result) {result}\n")
+            # resulting message post cipher op completion 
+
+            print("-" * 40)
+
+    def save_history(self, filename = "cipher_history.txt"):
+        '''Saves the history of all encryption and decryption operations to a text file.'''
+        # defining method in Cipher class to save history to file
+        # sets default filename to cipher_history.txt if user doesn't give different one 
+        # self gives access to data and attributes for history 
+
+        with open(filename, "w", encoding = "utf-8") as file:
+            # opens file with name provided in filename 
+            # with makes sure it properly closes afterwards 
+            # "w" means write and create file or overwrite if already exists 
+            # need the encoding utf-8 because of the cyrillic letters 
+            # file is handle used for the opened/created file here
+
+            for cipher_op, message, result in self._history:
+                # loops through everything in the history list 
+                # each item is a tuple of 3: 
+                # cipher_op: encrypt or decrypt, message, result 
+
+                file.write(f"{cipher_op}:\n")
+                # writes operation type to the file 
+                # \n goes to next line for formatting 
+
+                file.write(f" Сообщение: (message) {message}\n")
+                # writes message to file 
+
+                file.write(f" Результат: (result) {result}\n\n")
+                # writes result after encryption or decryption to file 
+
+        print(f"История сохранена в файл: {filename}")
+        print(f"History saved to file: {filename}")
+        # confirmation messages
+        
+    def export_to_csv(self, filename = "cipher_history.csv"):
+        '''Exports the encryption and decryption history to a CSV file.'''
+        # defines method called export_to_csv inside the Cipher class 
+        # takes one argument- filename 
+            
+        with open(filename, "w", newline ='', encoding = "utf-8") as file:
+        # opens in write mode 
+        # newline = '' will prevent extra blank lines in the spreadsheet 
+        # need the utf-8 encoding to preserve Russian characters 
+        # with closes file automatically after writing is completed 
+        
+            writer = csv.writer(file)
+            # creating csv writer object 
+          
+            writer.writerow(["Operation", "Message", "Result"])
+            # spreadsheet column names 
             
             for cipher_op, message, result in self._history:
-                # loops through info stored in self._history
-                # unpacking tuples into three variables 
-                
-                print(f"{cipher_op}:\n")
-                # printing cipher operation ie- Encrypt or Decrypt 
-                
-                print(f"  Message: {message}\n")
-                # starting message that operation will be performed on 
-                
-                print(f"  Result:   {result}\n")
-                # resulting message post cipher op completion 
-                
-                print("-" * 40)
-                
+            # going through the self._history list of tuples 
+            # tuple = cipher_op, message, result 
+            
+                writer.writerow([cipher_op, message, result])
+                # writing each tuple in the rows 
+        
+        print(f"История экспортирована в CSV: {filename}")
+        print(f"History exported to CSV: {filename}")
+        # confimration messages 
+        
+    def clear_history(self):
+        '''Clears the encryption and decryption history.'''
+        # defines method called clear_history in Cipher class
 
-# test example 
+        self._history = []
+        # this line essentially erases the history back to an empty list
+        # will only erase history for current object not all objects in class 
 
-cipher = Cipher(shift = 5, case_sensitive = True)
-
-encrypted = cipher.encrypt("Кто не рискует, тот не пьет шампанского")
-print("Encrypted:", encrypted)
-print("\n")
-
-
-decrypted = cipher.decrypt(encrypted)
-print("Decrypted:", decrypted)
-print("\n")
-
-print("*** History Ops Log ***\n")
-cipher.show_history()
+        print("История очищена. History cleared.")
+        # confirmation message
+    
--- Main.py
+++ Main.py
@@ -10,13 +10,296 @@
 # Modified Nov 5, 2025
 
 import Cipher
+# importing the cipher.py file for use that is in same directory 
+# the cipher file contains class- Cipher with two methods: encrypt, decrypt 
 
 def main():
-    # Code starts here
-    cipher = Cipher.Cipher()
-    cipher.encrypt("")
-    cipher.decrypt("")
-    pass
+    # defining function called main 
+    # it is the function where the cipher programs logic will run 
+    
+    cipher = Cipher.Cipher(shift = 5, case_sensitive = True)
+    
+    # this initializes a new Cipher object/instance of the Cipher class 
+    # assign and store it to a variable called cipher 
+    
+    # Cipher.Cipher means access that file that contains the class
+    # Cipher() - calls constructor of the class to create new object
+    
+    # full lines means to create a new instance/object based on the blueprint class in Cipher.py
+    # stored it in new variable called cipher to run encrypt/decrypt operations 
+    
+    
+    preset_messages_pairs = [
+        ("Кто не рискует, тот не пьет шампанского", "He who doesn't take risks doesn't drink champagne")
+        ("Работа не волк— в лес не убежит." , "Work isn't a wolf- it won’t run away into the forest.")
+        ("Слово не воробей: вылетит — не поймаешь", "A word is not a sparrow: once it flies out, you won’t catch it.") 
+        ("двумя зайцами погонишься, ни одного не поймаешь." , "If you chase two hares, you will not catch either.")
+        ("Без труда не вытащишь и рыбку из пруда." , "Without effort, you won’t even pull a fish out of a pond.")
+        ("Авось да небось до добра не доведут." , "Maybe and perhaps, won’t lead to any good." )
+        ("Доверяй, но проверяй." , "Trust but verify.")
+        ("Что посеешь, то и пожнёшь." , "You reap what you sow.")
+        ("В тихом омуте черти водятся.", "Still waters run deep with devils")
+        ("Не всё то золото, что блестит." , "Not all that glitters is gold.")
+        ("Повторение — мать учения." , "Repetition is the mother of learning.")
+        ("Не попробуешь — не узнаешь" , "If you don’t try, you’ll never find out.")
+        
+    ]
+
+    while True:
+            
+        print("--------------------------------------")
+        print(" ***** Меню Шифра / Cipher Menu ***** " )
+        print("--------------------------------------\n")
+        
+        print("1. Зашифровать сообщение / Encrypt a message")
+        print("2. Расшифровать сообщение / Decrypt a message")
+        print("3. Зашифровать заранее заданное сообщение / Encrypt a predefined message")
+        print("4. Показать историю / Show history")
+        print("5. Сохранить историю в текстовый файл / Save history to text file")
+        print("6. Экспортировать историю в CSV / Export history to CSV")
+        print("7. Очистить историю / Clear history")
+        print("8. Выход / Exit")
+        
+        choice = input("Выберите опцию (select an option) 1–8: ").strip()
+        # asks user to make selection from menu 1-8 
+        # .strip removes white space 
+        # user selection is stored in variable choice 
+        
+        if choice == "1": 
+        # if 1 then program performs encryption on text that user inputs 
+            user_message = input("Ввод текста: (input text) ")
+            
+            while True: 
+            # will ask user for a valid shift number for encypting 
+            # will only break once a valid number is recieved 
+            
+                shift_by = input("Введите номер смены: (enter shift number) ")
+                # asking user to input a number 
+                # where number user input is stored 
+               
+                try: 
+                # used for error catching 
+                # will try to convert input into an interger 
+                # if sucessful it will break 
+                # if failed moves to exception block for error handling 
+                     
+                    shift = int(shift_by)
+                    # converting input to interger
+                    
+                    break
+                    # if successful then break out of try block and continue with program 
+                    
+                except ValueError:
+                # failed converstion to integer then ValueError is raised 
+                
+                    print("Пожалуйста введите допустимое число, Please Enter A Valid Number")
+                    # printing correction message to the user to try again 
+                    # will keep looping until the user gets it right 
+            
+            while True:
+            # second loop asking the user if they want to keep case sensitivity or not
+            # will continue until user gives a valid input 
+            
+                try:
+                    case_selection = input("сохранить футляр, да/нет: (save the case, yes/no): ").strip().lower()
+                    # asks user to input yes or no (english or russian)
+                    # .strip() removes white space before and after input 
+                    # .lower() puts all input in lowercase for comparison of valid responses  
+                    
+                    if case_selection in ("да", "yes", "y"):
+                        case_sensitive = True
+                        break
+                        # user does wnat to preserve case sensitivty of original message
+                        # exit the loop and proceed  
+                        
+                    elif case_selection in ("нет", "no", "n"):
+                        case_sensitive = False
+                        break
+                        # user does not want to preserve case sensitivity of original message 
+                        # exit loop and proceed 
+                        
+                    else:
+                        raise ValueError("Неверный ответ, Invalid response")
+                        # if no input matches valid options ValueError is raised 
+                        # moving to the exception block instead of crashing out 
+                        
+                except ValueError:
+                # catching the error from else block 
+                
+                    print("Пожалуйста, введите 'да', 'нет', 'yes', 'no', 'y' или 'n'")
+                    print("Please enter 'yes', 'no', 'да', 'нет', 'y' or 'n'")
+                    # printing for the user what happened 
+                    # lets the keep trying until they figure out the correct input 
+            
+            user_cipher = Cipher.Cipher(shift = shift, case_sensitive = case_sensitive)
+            # creating another instance of the Cipher class based on user input 
+            # takes 2 arguments shift and case_sensitive 
+            # stored in user_cipher 
+
+            user_encrypted = user_cipher.encrypt(user_message)
+            # calls the encrypt() method to use on user_cipher object 
+            # passing user_message 
+            # storing in user_encrypted 
+
+            user_decrypted = user_cipher.decrypt(user_encrypted)
+            # calling decrypt method on user_cipher object 
+            # passes encrypted message 
+            # stores decrypted message in user_decrypted 
+
+            print("\nОригинал: (original)", user_message)
+            print("Зашифрованный: (encrypted)", user_encrypted)
+            print("Расшифрованный: (decrypted)", user_decrypted)
+            print()
+
+            print("*** История операций (History Ops Log) ***\n")
+            
+            user_cipher.show_history()
+            # calling the show_history method from cipher class for this object 
+            # will show all operations performed on user_cipher object/instance
+            # lets the user track what they just did in the program 
+
+        elif choice == "2":
+        # if option 2 is selcted by the user decryption is performed 
+        
+            encrypted_message = input("Введите зашифрованное сообщение: (enter encrypted message) ")
+            # user must enter a message that is already encrypted 
+            # user could encrypt a message and then select this option to decrypt afterwards 
+            # stored in encrypted_message variable 
+            
+            decrypted = cipher.decrypt(encrypted_message)
+            # uses the decrypt() method from the cipher class 
+            # taking encrypted_message and returning original back to user 
+            # stored in variable decyrpted 
+            
+            print("Расшифрованный: (decrypted)", decrypted)
+
+        elif choice == "3":
+        # checking if option 3 then encrytping and decrypting predefined messages 
+        # these messages are already stored in the program 
+        
+            message, translation = preset_messages_pairs[0]
+            # retrieving first message pair stored in list preset_message_pairs
+            # unpacking the tuple into variables 
+            # message gets Russian text
+            # translation English 
+            
+            print("Оригинальный текст на русском языке: ", message)
+            # printing original Russian Text for user 
+            
+            print("English Translation of Original Text: ", translation)
+            # printing english translation of original text for user 
+            
+            print()
+
+            encrypted = cipher.encrypt(message)
+            # encrypts russian message using encryt() method from cipher class 
+            # storing result in encrypted variable
+            # will use default cipher settings 
+            # shift = 5, case_senstivie = True
+            
+            print("зашифрованный: (encrypted)", encrypted)
+            print("\n")
+            # prints encrypted version 
+
+            decrypted = cipher.decrypt(encrypted)
+            # decrpyting message back to original form using decrypt() from cipher class 
+            
+            print("расшифрованный: (decrypted)", decrypted)
+            print("\n")
+            # printing decrypted message for user 
+
+            print("*** журнал истории ***\n")
+            cipher.show_history()
+            print("\n")
+            # calling show_history() method from Cipher class 
+            # will show list of all encryption and decryption on this cipher object 
+
+        elif choice == "4":
+        # checks if option 4 was selected by the user 
+        # option 4 is show history 
+        
+            cipher.show_history()
+            # calls the show_history() method from Cipher class 
+            # will show log of all operations performed on this cipher object 
+            # record of activity 
+
+        elif choice == "5":
+        # if option 5 then saving cipher history to a txt file 
+        
+            filename = input("Введите имя файла (или нажмите Enter для стандартного): ").strip()
+            # asks user to enter a file name 
+            # .strip() removes all leading and trailing white spaces in the input 
+            # user can also hit enter for a default file name to be applied 
+            # result stored in filename variable 
+            
+            cipher.save_history(filename if filename else "cipher_history.txt")
+            # saving cipher history to a txt file 
+            # calling the save_history() method from the Cipher class 
+            # filename is based on either user input or default cipher_history.txt
+            # filename if filename else means use what the user inputted for a file name 
+            # else if empty (user hit enter) then use default listed 
+
+        elif choice == "6":
+        # if user selected 6 then exporting to csv file 
+        
+            filename = input("Введите имя CSV файла (или нажмите Enter для стандартного): ").strip()
+            # asks user to enter a file name 
+            # .strip() removes all leading and trailing white spaces in the input 
+            # user can also hit enter for a default file name to be applied 
+            # result stored in filename variable
+            
+            cipher.export_to_csv(filename if filename else "cipher_history.csv")
+            # saving cipher history to a csv file
+            # calling the export_to_csv() method from the Cipher class 
+            # saves the history in a structured format defined in Cipher class
+            # filename if filename else means use what the user inputted for a file name 
+            # else if empty (user hit enter) then use default cipher_history.csv
+            
+      
+        elif choice == "7":
+            # if user selects 7 then clearing all cipher object history 
+            confirm = input("Вы уверены? да/нет, Are You Sure? yes/no: ").strip().lower()
+            # asks the user if they are sure they want to clear the history 
+
+            if confirm in ("да", "yes", "y"):
+                # if confirmed then clear 
+                # if they put no or anything else this will be skipped and nothing will be cleared 
+                cipher.clear_history()
+                # clear_history method is called from cipher class 
+               
+                print("История очищена, History cleared.")
+                # confirmation message 
+                
+            else:
+                print("История сохранена, History preserved.")
+                # if user changed their mind confirmation nothing deleted 
+        
+
+        elif choice == "8":
+            print("До свидания! / Goodbye!")
+            break
+
+        else:
+            print("Неверный выбор. Пожалуйста, выберите число от 1 до 8")
+            print("Wrong Choice. Please Select A Number 1-8")
+
+# -----------------------------------------------------
+#   ***** Run the Program *****
+# -----------------------------------------------------
 
 if __name__ == "__main__":
     main()
+    
+    # used to find out if the file is being run directly by the user 
+    # __name__ is built in python variable 
+    # if file is run directly (using .py) __name__ becomes __main__ 
+    # when imported from another then __name__ takes name of import file 
+    
+    # == "__main__" is checking if __name__ is equal to __main__
+    # if it is equal then directly running and use code in block 
+    # if imported this block does not execute
+
+
+
+
+
